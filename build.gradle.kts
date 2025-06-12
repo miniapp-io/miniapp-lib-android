2c2cc526f81514133a31282f059683faa49bdf26
biu <biu@mtsocialdao.com>
2025-06-12 17:58:12 +0800

    1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ nexusPublishing {
     }
 }
 
-ext["versionCode"] = 2
-ext["versionName"] = "1.0.1"
+ext["versionCode"] = 3
+ext["versionName"] = "1.0.2"
 ext["compileSdk"] = 35
 ext["targetSdk"] = 35
 ext["minSdk"] = 24
