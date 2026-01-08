c62f59f02dde8e595013f6d358939e5d9829f5e2
biu <biu@mtsocialdao.com>
2026-01-08 17:40:02 +0800

    fixed: cache webview

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ nexusPublishing {
     }
 }
 
-ext["versionCode"] = 28
-ext["versionName"] = "1.0.28"
+ext["versionCode"] = 29
+ext["versionName"] = "1.0.29"
 ext["compileSdk"] = 35
 ext["targetSdk"] = 35
 ext["minSdk"] = 24
